7dc62d407120cd133d2a76ef85ef24cb14e63ecf
Nikita Tulin <ntulin.work@gmail.com>
2024-06-15 16:05:58 -0300

    Replace text player hp text with sprite

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI playerHPText;
+    private Image playerHPImage;
 
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
+    private float defaultHPImageWidth;
+
+    private void Awake()
+    {
+        defaultHPImageWidth = playerHPImage.rectTransform.sizeDelta.x;
+    }
+
     public void UpdateHPText(int value)
     {
-        playerHPText.text = $"LIVES: {value}";
+        playerHPImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, defaultHPImageWidth * value);
     }
 
     public void UpdateScoreText(int value)
