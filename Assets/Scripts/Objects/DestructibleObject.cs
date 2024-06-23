df2db509c3eb214192e1358b81517a8c36dc6637
Nikita Tulin <ntulin.work@gmail.com>
2024-06-22 22:34:47 -0300

    Add sounds to destructible objects

--- Assets/Scripts/Objects/DestructibleObject.cs
+++ Assets/Scripts/Objects/DestructibleObject.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class DestructibleObject : HealthSystem
 {
+    [SerializeField]
+    private Behaviour[] BehavioursToDisable;
+
+    [SerializeField]
+    private AudioManager audioManager;
+
+    [SerializeField]
+    private string playAudioOnDied;
+
     private DropLootSystem lootSystem;
 
     protected override void Awake()
@@ -18,11 +27,28 @@ public class DestructibleObject : HealthSystem
         }
     }
 
-    private void OnDied(HealthSystem healthSystem)
+    private async void OnDied(HealthSystem healthSystem)
     {
         if (lootSystem!= null)
         {
             lootSystem.DropLoot();
+        }
+
+        if (playAudioOnDied != null && audioManager != null)
+        {
+            audioManager.Play(playAudioOnDied);
+
+            if (BehavioursToDisable != null)
+            {
+                for (int i = 0; i < BehavioursToDisable.Length; i++)
+                {
+                    BehavioursToDisable[i].enabled = false;
+                }
+            }
+
+            GetComponent<SpriteRenderer>().enabled = false;
+
+            await audioManager.WaitToFinishAll();
         }
 
         Destroy(gameObject);
