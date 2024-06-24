21d8ece9e8b95edeb7fdaf17d890e5a79e34d362
Nikita Tulin <ntulin.work@gmail.com>
2024-06-24 02:30:20 -0300

    Add health collect sfx

--- Assets/Scripts/Objects/Health.cs
+++ Assets/Scripts/Objects/Health.cs
@@ -7,6 +7,18 @@ public class Health : MonoBehaviour
     [SerializeField]
     private int healthToRecover = 1;
 
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GetComponent<AudioManager>();
+    }
+
+    private void Start()
+    {
+        audioManager.Play("Default", "Loot");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out PlayerHealthSystem player))
@@ -14,6 +26,8 @@ public class Health : MonoBehaviour
             Debug.Log("Player collected health");
 
             player.Heal(healthToRecover);
+
+            audioManager.SpawnTempAudioSourceAndPlay("Default", "Eat");
 
             Destroy(gameObject);
         }
